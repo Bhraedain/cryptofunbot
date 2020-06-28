630f35bc207f07e002c0d280122fc1de13462a57
Bhraedain <36554881+Bhraedain@users.noreply.github.com>
2020-06-28 18:54:58 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,28 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const { prefix, token } = require('./config.json');
+
+client.once('ready', () => {
+	console.log('Ready!');
+});
+
+// client.on('message', message => {
+    if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+    const args = message.content.slice(prefix.length).split(' ');
+    const command = args.shift().toLowerCase();
+    // the rest of your code
+
+client.on('message', message => {
+    
+	if (message.content === `${prefix}ping`) {
+        message.channel.send('Pong.');
+    } else if (message.content === `${prefix}server`) {
+        message.channel.send(`Server name: ${message.guild.name}\nTotal members: ${message.guild.memberCount}`);
+    }
+    else if (message.content === `${prefix}user-info`) {
+        message.channel.send(`Your username: ${message.author.username}\nYour ID: ${message.author.id}`);
+    }
+});
+
+client.login(token);
